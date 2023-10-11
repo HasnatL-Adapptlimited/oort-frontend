4efd0a74dc5233c3d64916991f55081ee8365ca3
HasnatL-Adapptlimited <hasnat.l@adappt.ai>
2023-10-11 15:02:56 +0100

    Sentry Setup frontend

--- apps/back-office/src/environments/environment.ts
+++ apps/back-office/src/environments/environment.ts
@@ -3,6 +3,9 @@ import { theme } from '../themes/default/default.local';
 import { sharedEnvironment } from './environment.shared';
 import { Environment } from './environment.type';
 
+/**
+ *
+ */
 const authConfig: AuthConfig = {
   issuer:
     'https://login.microsoftonline.com/76d22fc8-2330-45cf-ab36-51074cf8f1e2/v2.0',
